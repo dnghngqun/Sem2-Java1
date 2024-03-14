f74dd9a66ea951b7e69301aace63e09094f98ae4
hongquan <danghongquanvtc1111@gmail.com>
2024-03-14 14:55:18 +0700

    codeAcademy day 1

---
+++ CodeAcademy/MyProfile.java
@@ -0,0 +1,9 @@
+public class MyProfile {
+    public static void main(String[] args) {
+        String name = "Quan";
+        int age = 18;
+        double desiredSalary = 1000000;
+        char gender = 'm';
+        boolean lookingForJob = true;
+    }
+}
