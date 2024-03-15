47d5ce5d9d46e24a70ba860d74bca46588b0b876
hongquan <danghongquanvtc1111@gmail.com>
2024-03-15 11:31:07 +0700

    codeAcademy

---
+++ CodeAcademy/Final.java
@@ -0,0 +1,8 @@
+public class Final {
+    public static void main(String[] args) {
+        // final giống như hằng, giống const trong js
+        final double pi = 3.14;
+        System.out.println(pi);
+
+    }
+}
