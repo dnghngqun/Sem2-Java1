7140535cfdd7e77349c99054abb9ea945ddab4e7
Hồng Quân <danghongquanvtc1111@gmail.com>
2024-03-19 13:32:22 +0700

    Ex1

--- Exercise/Ex1_1/src/Client.java
+++ Exercise/Ex1_1/src/Client.java
@@ -1,6 +1,23 @@
+import static java.lang.System.out;
+
+/**
+ * Lớp Client sử dụng lớp Student để xây dựng chương trình java theo kịch bản.
+ *
+ * @author (Dang Hong Quan)
+ * @version (21.0.2)
+ */
+
 public class Client {
     public static void main(String[] args) {
-        Students student = new Students(1, "Quan", true);
-        student.printInfo();
+        Students student1 = new Students(1, "Quan", false);
+        student1.printInfo();
+        Students student2 = new Students(2, "Hoa", true);
+        Students student3 = new Students(3, "Hai", false);
+        student2.printInfo();
+        out.println("Student3 after setting gender and name");
+        student3.setMale(true);
+        student3.setName("Huy");
+        student3.printInfo();
+
     }
 }
---
+++ Exercise/Ex1_1/src/Students.java
@@ -0,0 +1,49 @@
+import static java.lang.System.out;
+public class Students {
+    private int id;
+    private String name;
+    private Boolean gender;//true is male, false is female
+
+    public Students(int id, String name, Boolean gender) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+    }
+
+    //getter
+    public int getId(){
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Boolean isMale() {
+        return this.gender;
+    }
+
+    //setter
+    public int setId(int id) {
+        return this.id = id;
+    }
+
+    public String setName(String name){
+        return this.name = name;
+    }
+
+    public Boolean setMale(Boolean gender){
+        return this.gender = gender;
+    }
+
+
+
+    public void printInfo(){
+        String genderConvert = this.isMale() ? "Male" : "Female";
+        out.println("-----------------------------------");
+        out.println("| ID | Name   | Gender |");
+        out.printf("| %-2d | %-6s | %-6s |\n", this.id, this.name, genderConvert);
+        out.println("-----------------------------------");
+
+    }
+}
