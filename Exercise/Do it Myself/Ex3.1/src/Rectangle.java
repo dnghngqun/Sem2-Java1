cbdfbc532eddaf24f614844aa4c5636a0b70f061
Hồng Quân <danghongquanvtc1111@gmail.com>
2024-03-21 10:04:24 +0700

    Do it Myself - Ex1

--- Exercise/Do it Myself/Ex3.1/src/Rectangle.java
+++ Exercise/Do it Myself/Ex3.1/src/Rectangle.java
@@ -12,32 +12,39 @@ public class Rectangle {
     public int getArea() {
         return width * height;
     }
+
     public int getHeight() {
         return height;
     }
+
     public int getWidth() {
         return width;
     }
+
     public void setWidth(int width) {
         this.width = width;
     }
+
     public void setHeight(int height) {
         this.height = height;
     }
+
     public int getPerimeter() {
         return 2 * (width + height);
     }
 
-    public void display(){
-        for(int i = 0; i < height; i++){
-            for(int j = 0; j < width; j++){
+    public void display() {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
                 out.print("# ");
             }
             out.print("\n");
         }
     }
-    public Rectangle(){
-        //has been written in main
+
+    public Rectangle() {
+        this.width = 7;
+        this.height = 3;
     }
 
 }
