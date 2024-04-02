5083c3d3f59535aef813bc94815e1d9eb5eff41e
Hồng Quân <danghongquanvtc1111@gmail.com>
2024-04-02 09:07:11 +0700

    InClass

---
+++ inClass/Try-catch/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+        * Khối lệnh try catch finally
+        * try{
+        * //code có thể ném ra ngoại lệ
+        * }catch(Exception_class_name e){
+        * //code xử lý ngoại lệ
+        * }finally{
+        * //code trong khối này luôn được thực thi
+        * }
+        *
+        * Một số phương thức(method) của lớp Exception
+        * getMessage(): trả về một message cụ thể về exception đã xảy ra
+        * getCause(): trả về nguyên nhân xảy ra exception
+        * toString(): trả về tên của lớp và kết hợp với kết quả từ phương thức getMessage()
+        * printStackTrace(): in ra kết quả của phương thức toString() cùng với stack trace đến System.err
+        * fillInStackTrace(): làm đầy stack trace của đối tượng Throwable bằng stack trace hiện tại
+        * */
+    }
+}
